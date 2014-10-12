c8f2cc16c10222290c58a2d61d948cd93dd3e3e8
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-10-12 16:47:08 -0600

    changes to smtp

--- config/initializers/pony_options.rb
+++ config/initializers/pony_options.rb
@@ -4,9 +4,9 @@ Pony.options = {
   via_options: {
     address:              "smtp.sendgrid.net",
     port:                 "587",
-    user_name:            ENV["wb9753"] ||
+    user_name:            ENV["SMTP_USERNAME"] ||
                           raise("Please set SMTP_USERNAME"),
-    password:             ENV["SG4em1M"] ||
+    password:             ENV["SMTP_PASSWORD"] ||
                           raise("Please set SMTP_PASSWORD"),
     authentication:       "plain",
     enable_starttls_auto: true,
